7e44fa89291402a13ae00b85848e92e04da6ce32
brianabernathy <brianabernathy@users.noreply.github.com>
2021-03-01 10:06:03 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# gfa_var_genotyper
+# gfa_var_genotyperUsage:
+
+    gfa_var_genotyper.pl [options]
+
+Options:
+
+  general options:
+
+     -v --var     gfa variants file (required)
+
+     -p --pack    vg pack table file(s) (required)
+
+     --ploidy     1 (haploid) or 2 (diploid) currently supported
+                    default: 1
+
+  genotyping options:
+
+    Genotyping thresholds can be set manually or dynamically by creating
+    coverage count histograms that are provide to GenomeScope for modeling.
+    (modeling is not recommended for GBS data) When modeling is enabled, any
+    samples that fail to generate a valid model will fallback to using the
+    static parameters.
+
+    Static parameter genotyping sets a minimum total coverage (all alleles)
+    threshold. The remaining variants are processed as either low or high
+    total coverage. Low coverage variants are genotyped using minimum allele
+    counts. High coverage variants are genotyped using minimum allele
+    percentages.
+
+   modeling:
+
+     -m --model   use genomescope model to define genotyping thresholds
+                    default: disabled
+
+     --modeldir   output directory for modeling files
+                    default: gfa_var_genotyper_models
+
+     --gs         path to GenomeScope executable
+
+   static parameters:
+
+     --min_tot_cov               minimum total coverage (all alleles)
+                                   default: 3
+
+     --max_low_cov_tot_cov       maximum "low coverage" total coverage
+                                   default: 9
+
+     --min_low_cov_allele_count  minimum "low coverage" allele count
+                                   default: 3
+
+     --min_high_cov_allele_pct   minimum "high coverage" allele percent
+                                   default: 10
+
+  help:
+
+     -h --help    display help menu
---
+++ gfa_var_genotyper.pl
@@ -0,0 +1,487 @@
+#!/usr/bin/env perl
+
+use Getopt::Long;
+use Pod::Usage;
+
+use strict;
+use warnings;
+
+my $gfa_var_file;
+my @pack_files = ();
+my %seg_lens = ();
+my $min_tot_cov = 3;
+my $max_low_cov_tot_cov = 9;
+my $min_low_cov_allele_count = 3;
+my $min_high_cov_allele_pct = 10;
+my $ploidy = 1;
+my $use_model = 0;
+my $model_dir = 'gfa_var_genotyper_models';
+my $gs_exec;
+my $kmer_size = 31;
+my $help;
+
+my %rec_var_seg_gts = ();
+my %rec_gt_var_segs = ();
+my %rec_id_allele_counts = ();
+my %pack_covs = ();
+my @file_bases = ();
+
+parse_args();
+parse_gfa_var_file();
+
+foreach my $pack_file (@pack_files) {
+	parse_pack_file($pack_file);
+}
+
+print_gfa_var_gts();
+
+exit(0);
+
+
+sub print_gfa_var_gts {
+	open(GFA_VARS, '<', $gfa_var_file) or error("$!");
+
+	while (my $line = <GFA_VARS>) {
+		chomp($line);
+
+		my ($chr, $pos, $id, $ref, $alts, $filter, $info, $format, @gts) = split(/\t/, $line);
+
+		if (! defined($id)) {
+			next();
+		}
+
+		if ($chr eq 'CHROM') {
+			print(STDOUT join("\t", $chr, $pos, $id, $ref, $alts, $filter, $info, $format, @gts, @file_bases), "\n");
+
+			next();
+		}
+
+		my $rec_id = "$chr\t$pos\t$id";
+		my $allele_count = $rec_id_allele_counts{$rec_id};
+
+		my $mod_line = $line;
+
+		$mod_line =~ s/GT/GT:AD/;
+
+		print(STDOUT "$mod_line");
+
+		foreach my $file_base (@file_bases) {
+			my %gt_cov = ();
+			my %gt_cov_order = ();
+			my $tot_cov = 0;
+			my @ads = ();
+
+			foreach my $gt (0..$allele_count - 1) {
+				my $cov = 0;
+
+				if (exists($rec_gt_var_segs{$rec_id}{$gt})) {
+					my $var_seg = $rec_gt_var_segs{$rec_id}{$gt};
+
+					if (exists($pack_covs{$file_base}{$var_seg})) {
+						$cov = $pack_covs{$file_base}{$var_seg};
+					}
+				}
+
+				$gt_cov{$gt} = $cov;
+				$gt_cov_order{$cov}{$gt}++;
+				$tot_cov += $cov;
+
+				push(@ads, $cov);
+			}
+
+			my $ref_gt = 0;
+			my $alt_gt = 1;
+			my $gt_rec = '.';
+
+			if ($ploidy == 2) {
+				$gt_rec .= '/.';
+			}
+
+			if ($allele_count > 2) {
+				# multiallelic: select top 2 cov alleles to represent ref/alt
+				my $count = 0;
+
+				foreach my $cov (reverse sort { $a <=> $b } keys %gt_cov_order) {
+					foreach my $gt (sort { $a <=> $b } keys %{$gt_cov_order{$cov}}) {
+						if ($count == 0) {
+							$ref_gt = $gt;
+						}
+
+						elsif ($count == 1) {
+							$alt_gt = $gt;
+						}
+
+						else {
+							last();
+						}
+
+						$count++;
+					}
+				}
+			}
+
+			if ($tot_cov < $min_tot_cov) {
+				# skip: low cov
+			}
+
+			else {
+				my $ref_cov = 0;
+				my $ref_cov_pct = 0;
+				my $alt_cov = 0;
+				my $alt_cov_pct = 0;
+
+				if (exists($gt_cov{$ref_gt})) {
+					$ref_cov = $gt_cov{$ref_gt};
+					$ref_cov_pct = $ref_cov / $tot_cov * 100;
+				}
+
+				if (exists($gt_cov{$alt_gt})) {
+					$alt_cov = $gt_cov{$alt_gt};
+					$alt_cov_pct = $alt_cov / $tot_cov * 100;
+				}
+
+				if ($tot_cov <= $max_low_cov_tot_cov) {
+					if ($ref_cov >= $min_low_cov_allele_count && $alt_cov >= $min_low_cov_allele_count) {
+						if ($ploidy >= 2) {
+							$gt_rec = "$ref_gt/$alt_gt";
+						}
+					}
+
+					elsif ($ref_cov >= $min_low_cov_allele_count) {
+						$gt_rec = "$ref_gt";
+
+						if ($ploidy == 2) {
+							$gt_rec = "./$ref_gt";
+						}
+					}
+
+					else {
+						$gt_rec = "$alt_gt";
+
+						if ($ploidy == 2) {
+							$gt_rec = "./$alt_gt";
+						}
+					}
+				}
+
+				else {
+					if ($ref_cov_pct >= $min_high_cov_allele_pct && $alt_cov_pct >= $min_high_cov_allele_pct) {
+						if ($ploidy >= 2) {
+							$gt_rec = "$ref_gt/$alt_gt";
+						}
+					}
+
+					elsif ($ref_cov_pct < $min_high_cov_allele_pct) {
+						$gt_rec = "$alt_gt";
+
+						if ($ploidy == 2) {
+							$gt_rec = "$alt_gt/$alt_gt";
+						}
+					}
+
+					else {
+						$gt_rec = "$ref_gt";
+
+						if ($ploidy == 2) {
+							$gt_rec = "$ref_gt/$ref_gt";
+						}
+					}
+				}
+			}
+
+			print(STDOUT "\t$gt_rec:", join(',', @ads));
+		}
+
+		print(STDOUT "\n");
+	}
+
+	return(0);
+}
+
+
+sub parse_pack_file {
+	my $pack_file = shift();
+
+	my %cov_histo = ();
+	my %seg_lens = ();
+	my %seg_covs = ();
+	my $file_base = $pack_file;
+
+	$file_base =~ s/^.*\///;
+	$file_base =~ s/\..*$//;
+
+	push(@file_bases, $file_base);
+
+	open(PACK, '<', $pack_file) or error("$!");
+
+	while (my $line = <PACK>) {
+		chomp($line);
+
+		if ($line =~ /^seq\.pos/) {
+			next();
+		}
+
+		my ($seq_pos, $node_id, $node_offset, $cov) = split(/\t/, $line);
+
+		if ($use_model == 1) {
+			if ($cov > 0) {
+				$cov_histo{$cov}++;
+			}
+		}
+
+		if (exists($seg_lens{$node_id})) {
+			if (($node_offset + 1) > $seg_lens{$node_id}) {
+				$seg_lens{$node_id} = $node_offset + 1;
+			}
+		}
+
+		else {
+			$seg_lens{$node_id} = $node_offset + 1;
+		}
+
+		$seg_covs{$node_id} += $cov;
+	}
+
+	close(PACK);
+
+	foreach my $node_id (keys %seg_covs) {
+		my $len = $seg_lens{$node_id};
+		my $cov = $seg_covs{$node_id};
+		my $avg_cov = int(($cov / $len) + 0.5);
+
+		$pack_covs{$file_base}{$node_id} = $avg_cov;
+	}
+	
+
+
+	if ($use_model == 1) {
+		my $hist_dir = "$model_dir/cov_hist";
+
+		if (! -e $hist_dir) {
+			mkdir($hist_dir) or error("$!");
+		}
+
+		my $cov_histo_file = "$hist_dir/$file_base.cov.histo";
+
+		open(COV, '>', $cov_histo_file) or error("$!");
+
+		foreach my $cov (sort { $a <=> $b } keys %cov_histo) {
+			print(COV "$cov\t$cov_histo{$cov}\n");
+		}
+
+		close(COV);
+
+
+		my $gs_dir = "$model_dir/genomescope";
+
+		if (! -e $gs_dir) {
+			mkdir($gs_dir) or error("$!");
+		}
+
+		my $gs_base = "$gs_dir/$file_base";
+
+		my $gs_cmd = "/home/bla/bin/genomescope2.0/genomescope.R -i $cov_histo_file -o $gs_dir -k $kmer_size -p $ploidy -n $file_base 1> $gs_base.gs.stdout 2> $gs_base.gs.stderr";
+
+		system($gs_cmd);
+	}
+
+	return(0);
+}
+
+
+sub parse_gfa_var_file {
+	open(GFA_VARS, '<', $gfa_var_file) or error("$!");
+
+	while (my $line = <GFA_VARS>) {
+		chomp($line);
+
+		my ($chr, $pos, $id, $ref, $alts, $filter, $info, $format, @gts) = split(/\t/, $line);
+
+		if (! defined($id)) {
+			next();
+		}
+
+		if ($chr eq 'CHROM') {
+			next();
+		}
+
+		my $type;
+
+		if ($ref =~ s/^complex\.//) {
+			$type = 'complex';
+		}
+
+		elsif ($ref =~ s/^multiPath\.//) {
+			$type = 'multipath';
+		}
+
+		else {
+			$type = 'snp';
+
+			($ref, $alts) = split('-', $id);
+		}
+
+		my @alts = split(',', $alts);
+		my @alleles = ($ref, @alts);
+		my $shared_seg = $pos;
+		my $rec_id = "$chr\t$pos\t$id";
+
+		$rec_id_allele_counts{$rec_id} = $#alleles + 1;
+
+		foreach my $gt (0..$#alleles) {
+			my $var_seg = $alleles[$gt];
+
+			$rec_gt_var_segs{$rec_id}{$gt} = $var_seg;
+			$rec_var_seg_gts{$rec_id}{$var_seg} = $gt;
+		}
+	}
+
+	close(GFA_VARS);
+
+	return(0);
+}
+
+
+sub error {
+	my $msg = shift();
+
+	if (defined($msg)) {
+		print(STDERR "error: $msg\n");
+	}
+
+	exit(0);
+}
+
+
+sub arg_error {
+	my $msg = shift();
+
+	if (defined($msg)) {
+		print(STDERR "error: $msg\n\n");
+	}
+
+	print("use option -h or --help to display help menu\n");
+
+	exit(0);
+}
+
+
+sub parse_args {
+	if ($#ARGV == -1) {
+		pod2usage(-exitval => 0, -verbose => 1);
+	}
+
+	GetOptions ('v|var=s' => \$gfa_var_file,
+				'p|pack=s{1,}' => \@pack_files,
+				'ploidy=i' => \$ploidy,
+				'm|model' => \$use_model,
+				'modeldir=s' => \$model_dir,
+				'gs=s' => \$gs_exec,
+				'min_tot_cov=i' =>\$min_tot_cov,
+				'max_low_cov_tot_cov=i' => \$max_low_cov_tot_cov,
+				'min_low_cov_allele_count=i' => \$min_low_cov_allele_count,
+				'min_high_cov_allele_pct=i' => \$min_high_cov_allele_pct,
+				'h|help' => \$help) or error('cannot parse arguments');
+
+	if (defined($help)) {
+		pod2usage(-exitval => 0, -verbose => 1);
+	}
+
+	if (! defined($gfa_var_file)) {
+		arg_error('gfa variants file required');
+	}
+
+	if (! @pack_files) {
+		arg_error('at least 1 pack table file required');
+	}
+
+	if ($ploidy != 1 && $ploidy != 2) {
+		arg_error('ploidy must be either 1 or 2');
+	}
+
+	if ($use_model == 1) {
+		$model_dir =~ s/\/$//;
+
+		if (! -e $model_dir) {
+			my $cmd = "mkdir -p $model_dir";
+
+			system($cmd);
+		}
+	}
+
+	return(0);
+}
+
+
+__END__
+
+=head1 NAME
+
+gfa_var_genotyper.pl
+
+=head1 SYNOPSIS
+
+gfa_var_genotyper.pl [options]
+
+=head1 DESCRIPTION
+
+gfa_var_genotyper.pl genotypes variants called directly from GFA files
+
+=head1 AUTHOR
+
+Brian Abernathy
+
+=head1 OPTIONS
+
+=head2 general options
+
+ -v --var     gfa variants file (required)
+
+ -p --pack    vg pack table file(s) (required)
+
+ --ploidy     1 (haploid) or 2 (diploid) currently supported
+                default: 1
+
+=head2 genotyping options
+
+Genotyping thresholds can be set manually or dynamically by creating
+coverage count histograms that are provide to GenomeScope for modeling.
+(modeling is not recommended for GBS data) When modeling is enabled,
+any samples that fail to generate a valid model will fallback to
+using the static parameters.
+
+Static parameter genotyping sets a minimum total coverage (all alleles)
+threshold. The remaining variants are processed as either low or high
+total coverage. Low coverage variants are genotyped using minimum
+allele counts. High coverage variants are genotyped using minimum
+allele percentages.
+
+=head3 modeling
+
+ -m --model   use genomescope model to define genotyping thresholds
+                default: disabled
+
+ --modeldir   output directory for modeling files
+                default: gfa_var_genotyper_models
+
+ --gs         path to GenomeScope executable
+
+=head3 static parameters
+
+ --min_tot_cov               minimum total coverage (all alleles)
+                               default: 3
+
+ --max_low_cov_tot_cov       maximum "low coverage" total coverage
+                               default: 9
+
+ --min_low_cov_allele_count  minimum "low coverage" allele count
+                               default: 3
+
+ --min_high_cov_allele_pct   minimum "high coverage" allele percent
+                               default: 10
+
+=head2 help
+
+ -h --help    display help menu
+
+=cut
